98efc39f60452b1574de8a62c3272a7718f78e05
Chaeyoung Ha <81142151+ChaeyoungHa@users.noreply.github.com>
2023-02-11 21:21:42 +0900

    add a Text component to show date

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,80 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Colors from '../constants/Colors';
+import Dimensions from '../constants/Dimensions';
 import { RootTabScreenProps } from '../types';
 
+import LeftArrow from '../assets/left-arrow.svg';
+import RightArrow from '../assets/right-arrow.svg';
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
-	return <View></View>;
+	return (
+		<View
+			style={{
+				backgroundColor: 'white',
+				width: Dimensions.width * 375,
+				height: Dimensions.height * 812,
+				paddingHorizontal: Dimensions.width * 16,
+				paddingTop: useSafeAreaInsets().top,
+				paddingBottom: useSafeAreaInsets().bottom,
+			}}
+		>
+			<View
+				style={{
+					marginVertical: Dimensions.height * 21,
+					width: Dimensions.width * 343,
+					height: Dimensions.height * 47,
+				}}
+			>
+				<Text
+					style={{
+						color: Colors.Green01,
+						fontSize: 12,
+					}}
+				>
+					February 3
+				</Text>
+				<View
+					style={{
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+						alignItems: 'center',
+					}}
+				>
+					<Text
+						style={{
+							color: Colors.Black,
+							fontSize: 24,
+							fontWeight: 'bold',
+						}}
+					>
+						Monday's Diet
+					</Text>
+					<View
+						style={{
+							flexDirection: 'row',
+							width: Dimensions.width * 50,
+							justifyContent: 'space-between',
+							alignItems: 'center',
+						}}
+					>
+						<Pressable
+							style={({ pressed }) => ({
+								opacity: pressed ? 0.5 : 1,
+							})}
+						>
+							<LeftArrow />
+						</Pressable>
+						<Pressable
+							style={({ pressed }) => ({
+								opacity: pressed ? 0.5 : 1,
+							})}
+						>
+							<RightArrow />
+						</Pressable>
+					</View>
+				</View>
+			</View>
+		</View>
+	);
 }
