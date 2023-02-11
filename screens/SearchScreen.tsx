c8b93eb848ae008c4e724c484f88c5a93c2232cf
Chaeyoung Ha <81142151+ChaeyoungHa@users.noreply.github.com>
2023-02-11 23:12:55 +0900

    Update SearchScreen

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,9 +1,98 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, Modal } from 'react-native';
 import { RootTabScreenProps } from '../types';
-
+import Dimensions from '../constants/Dimensions';
+import Colors from '../constants/Colors';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import RightArrow from '../assets/right-arrow.svg';
+import { useState } from 'react';
 export default function SearchScreen({
 	navigation,
 }: RootTabScreenProps<'Search'>) {
-	return <View></View>;
+	const [type, setType] = useState('Row Food Vegan');
+	const [modalVisible, setModalVisible] = useState(false);
+	return (
+		<>
+			<Modal animationType='fade' transparent={true} visible={modalVisible}>
+				<View
+					style={{
+						backgroundColor: 'white',
+						width: Dimensions.width * 375,
+						height: Dimensions.height * 812,
+						paddingHorizontal: Dimensions.width * 16,
+						paddingTop: useSafeAreaInsets().top,
+						paddingBottom: useSafeAreaInsets().bottom,
+					}}
+				></View>
+			</Modal>
+			<View
+				style={{
+					backgroundColor: 'white',
+					width: Dimensions.width * 375,
+					height: Dimensions.height * 812,
+					paddingHorizontal: Dimensions.width * 16,
+					paddingTop: useSafeAreaInsets().top,
+					paddingBottom: useSafeAreaInsets().bottom,
+				}}
+			>
+				<View
+					style={{
+						width: Dimensions.width * 375,
+						height: Dimensions.height * 104,
+						justifyContent: 'center',
+					}}
+				>
+					<Pressable
+						style={({ pressed }) => ({
+							opacity: pressed ? 0.5 : 1,
+							width: Dimensions.width * 343,
+							height: Dimensions.height * 59,
+							borderColor: '#C5C4C4',
+							borderWidth: 1,
+							flexDirection: 'row',
+							justifyContent: 'space-between',
+							alignItems: 'center',
+							paddingHorizontal: Dimensions.width * 15,
+							borderRadius: 6,
+						})}
+						onPress={() => setModalVisible(true)}
+					>
+						<Text
+							style={{
+								color: Colors.Black,
+								fontSize: 16,
+							}}
+						>
+							{type}
+						</Text>
+						<RightArrow />
+					</Pressable>
+				</View>
+				<View
+					style={{
+						width: Dimensions.width * 375,
+						height: Dimensions.height * 8,
+						backgroundColor: '#F8F4F4',
+					}}
+				/>
+				<View
+					style={{
+						width: Dimensions.width * 375,
+						backgroundColor: 'white',
+						paddingTop: Dimensions.height * 19,
+					}}
+				>
+					<Text
+						style={{
+							color: Colors.Black,
+							fontSize: 24,
+							fontWeight: 'bold',
+						}}
+					>
+						Vegan Recipies
+					</Text>
+				</View>
+			</View>
+		</>
+	);
 }
